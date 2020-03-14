39186d4da719fd6ff182cbfa09ddeeb78c023dc4
bensmith <benjaminsmith1981@gmail.com>
2020-03-14 22:39:46 +0000

    * .gitignore: new coding
    * CodingCheck.sln:
    * Program.cs:
    * CodingCheck.csproj:

---
+++ CodingCheck/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CodingCheck
+{
+
+    class Program
+    {
+        static int shipSpeed = 10;
+
+        public void increaseSpeed()
+        {
+
+            shipSpeed = 10;
+
+            string[] names = {"Bee","Thomas"};
+            for (int index = 0;
+                index < names.Length;
+                index++)
+            {
+                Console.WriteLine(names[index]);
+            }
+
+        }
+
+        public static void decreaseSpeed(int increase)
+        {
+            shipSpeed = shipSpeed + increase;
+
+        }
+        static void Main(string[] args)
+        {
+            decreaseSpeed(5);
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
